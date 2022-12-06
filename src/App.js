1418509f54e12cd8391485e5663348f3c5a957fc
Franco Quiroga <francoags02@gmail.com>
2022-12-06 15:18:39 -0300

    Segunda entrega proyecto final

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './App.css';
 import ItemListContainer from './components/ItemListContainer';
 import NavBar from './components/NavBar';
-
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
+
   return (
-    <div>
+    <BrowserRouter>{/* A trabajar */}
       <NavBar/>
       <ItemListContainer greeting={"¡Krid Argentina tienda virtual de moda y deporte!"}/>
-    </div>
+    </BrowserRouter>
   );
 }
 
