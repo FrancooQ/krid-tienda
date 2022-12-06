6fc159e111dc36566f7140d204747fb4172f0687
Franco Quiroga <francoags02@gmail.com>
2022-12-06 15:25:46 -0300

    Primera Entrega React

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div>
       <NavBar/>
-      <ItemListContainer greeting={"¡Krid Argentina tienda virtual de moda y deporte!"}/>
+      <ItemListContainer greeting={"¡Krid Argentina tienda virtual de moda y deporte!!"}/>
     </div>
   );
 }
